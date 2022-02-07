1a5839f7b293a2aa6da99c62d12c8e720bbd981a
Nir <nchotwani24041992@gmail.com>
2022-02-07 10:08:32 -0800

    first commit

---
+++ src/test/java/test/firstClass.java
@@ -0,0 +1,11 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class firstClass {
+
+	@Test(priority=0)
+	public void firstMethod(){
+		System.out.println("Hi I am firtMethod");
+	}
+}
